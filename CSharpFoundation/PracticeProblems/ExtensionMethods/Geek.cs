99a852393cba68b3584d3a9a3214d1566f8707cb
SamuelSherpa <sas2732@my.londonmet.ac.uk>
2024-02-21 19:24:27 +0545

    Add - Code for Extension methods

---
+++ CSharpFoundation/PracticeProblems/ExtensionMethods/Geek.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFoundation.PracticeProblems.ExtensionMethods
+{
+    public class Geek
+    {
+        public void M1()
+        {
+            Console.WriteLine("Method name: M1");
+        }
+
+        public void M2()
+        {
+            Console.WriteLine("Method name: M1");
+        }
+
+        public void M3()
+        {
+            Console.WriteLine("Method name: M3");
+        }
+    }
+}
---
+++ CSharpFoundation/PracticeProblems/ExtensionMethods/SecondGeekClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFoundation.PracticeProblems.ExtensionMethods
+{
+     static class SecondGeekClass
+    {
+        public static void M4(this Geek g)
+        {
+            Console.WriteLine("Method name: M4");
+        }
+        public static void M5(this Geek g, string str)
+        {
+            Console.WriteLine($"The given parameter string is: {str}");
+        }
+    }
+}
