a22d46b6d3896ee72333051ec95866bc4ff6333b
SamuelSherpa <sas2732@my.londonmet.ac.uk>
2024-02-27 20:21:40 +0545

    Add code for NullableReference types.

---
+++ CSharpFoundation/PracticeProblems/NullableReferenceTypes/NullableReferenceTypes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFoundation.PracticeProblems.NullableReferenceTypes
+{
+    public class NullableReferenceTypes
+    {
+        public static void Main(string[] args)
+        {
+            string? Message = null;
+
+            if(!string.IsNullOrEmpty(Message))
+            {
+            Console.WriteLine($"The length of the Message is {Message.Length}\n");
+            }
+            //Console.WriteLine($"The length of the Message is {Message.Length}\n");
+
+            var originalMessage = Message;
+
+            Message = "Hello, World";
+            Console.WriteLine($"The length of the Message is {Message}");
+
+            Console.WriteLine($"The length of the originalMessage is: {originalMessage.Length}");
+        }
+    }
+}
