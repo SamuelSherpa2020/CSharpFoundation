977ed3c466d6ae4e854400ca3d3771a609f69296
SamuelSherpa <sas2732@my.londonmet.ac.uk>
2024-04-15 09:11:32 +0545

    Learn about null coalescing with strings.

---
+++ CSharpFoundation/PracticeProblems/StringLearn/StringLearn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFoundation.PracticeProblems.StringLearn
+{
+    public class StringLearn
+    {
+        public static void Main(string[] args)
+        {
+            string fruit = "apple";
+            string person = null;
+            string myString = null;
+
+            Console.WriteLine($"Fruit name is {(fruit?? "No fruit")}");
+            Console.WriteLine($"Person name is {(person !=null ? "No person":"Samuel")}");
+            Console.WriteLine($"Person name is {(myString?.Length ??0)}");
+            Console.ReadLine();
+        }
+    }
+}
