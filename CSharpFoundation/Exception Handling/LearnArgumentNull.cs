c339799df8d496ad12cfb4fc7acfc55bc0a89d2f
SamuelSherpa <sas2732@my.londonmet.ac.uk>
2024-04-15 09:18:32 +0545

    Add code to study about exception handling in ArgumentNull

---
+++ CSharpFoundation/Exception Handling/LearnArgumentNull.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFoundation.Exception_Handling
+{
+    public class LearnArgumentNull
+    {
+        public static void Main(string[] args)
+        {
+            GiveName("");
+            Console.ReadLine();
+        }
+
+        public static void GiveName(string chilling)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(chilling))
+                {
+                    throw new ArgumentException(nameof(chilling));
+                }
+                else
+                {
+                    Console.WriteLine(chilling);
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch
+            {
+                Console.WriteLine("An unexpected error occurred.");
+            }
+        }
+    }
+}
