61e6edcf3983079d3c4ecfd1e1dd3bf46f7b11fe
King_Jireh <samuelsherpa46@gmail.com>
2024-07-03 22:16:03 +0545

    Learn and Complete code for IEnumerable and ICollection. In detail study of the interfaces.

---
+++ CSharpFoundation/LearnCollections/LearnEnumerable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+
+namespace CSharpFoundation.LearnCollections;
+
+public class LearnEnumerable
+{
+    public static void Main(string[] args)
+    {
+        var cities = new List<string>() { "New York", "London", "Tokyo", "Lisbon", "Hyderabad", "Chicago" };
+
+        IEnumerable<string> query = cities.Where(x => x.StartsWith("L"));
+
+        IEnumerator<string> getEnumerator = query.GetEnumerator();
+        while (getEnumerator.MoveNext())
+        {
+            string city = getEnumerator.Current;
+            Console.WriteLine(city);
+        }
+        foreach (var c in query)
+            Console.WriteLine(c);
+    }
+}
---
+++ CSharpFoundation/LearnCollections/LearnICollection.cs
@@ -0,0 +1,45 @@
+﻿
+
+using Microsoft.VisualBasic;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+
+namespace CSharpFoundation.LearnCollections;
+
+public class LearnICollection
+{
+    public static void Main()
+    {
+        ICollection<string> countries = new Collection<string>();
+        ICollection<string> states = new List<string>();
+        
+        //Adding Countries
+        countries.Add("Nepal");
+        countries.Add("Russia");
+        countries.Add("USA");
+        foreach (string country in countries)
+            Console.WriteLine(country);
+       
+        Console.WriteLine(countries.Count);
+        Console.WriteLine();
+        
+        countries.Remove("Russia");
+        foreach (string country in countries)
+            Console.WriteLine(country);
+        countries.Clear();
+        
+        Console.WriteLine("After Clearing Countries");
+
+        foreach (string country in countries)
+            Console.WriteLine(country);
+
+
+        //Adding States
+        states.Add("Koshi");
+        states.Add("Amur");
+        states.Add("Colorado");
+
+        foreach (string state in states)
+            Console.WriteLine(state);
+    }
+}
