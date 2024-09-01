5b1c615818e64e670c3a98a0dd198584eb25480f
King_Jireh <samuelsherpa46@gmail.com>
2024-09-01 22:06:17 +0545

    Add a simple program to reverse the given string.

---
+++ CSharpFoundation/PracticeProblems/ReverseGivenString.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFoundation.PracticeProblems
+{
+    public class ReverseGivenString
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string: ");
+            string stringToReverse = Console.ReadLine()!;
+            char[] words = stringToReverse.ToCharArray();
+            string reversedString = string.Empty;
+
+            for (int i = words.Length-1; i >= 0; i--)
+            {
+                reversedString += words[i];
+            }
+            Console.WriteLine($"The reverse of the string is :{reversedString}");
+        }
+    }
+}
