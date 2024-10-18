e5ef3b46d218f0b7e7d9494548d19aa7e39e787e
King_Jireh <samuelsherpa46@gmail.com>
2024-10-18 21:15:39 +0545

    Reverse the order of given words. New simple problem solved.

---
+++ CSharpFoundation/PracticeProblems/ReverseGivenWords.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFoundation.PracticeProblems
+{
+    public class ReverseGivenWords
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter your sentence: ");
+            string? givenSentence = Console.ReadLine();
+            string[] givenWords = givenSentence!.Split(' ');
+            string reversedWords = string.Empty;
+            
+            for (int i = givenWords.Length - 1; i >= 0; i--)
+            {
+                reversedWords  += givenWords[i] +  " ";
+            }
+            Console.WriteLine(reversedWords);
+        }
+    }
+}
