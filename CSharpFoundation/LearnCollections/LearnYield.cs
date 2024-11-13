af7e46cf5da3b5ffba8207f7560a76f09c9f194e
King_Jireh <samuelsherpa46@gmail.com>
2024-11-13 15:16:03 +0545

    learning yield

---
+++ CSharpFoundation/LearnCollections/LearnYield.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFoundation.LearnCollections
+{
+    public class LearnYield
+    {
+        public static void Main(string[] args)
+        {
+            IEnumerable<int> nums = GenerateNumbers(5);
+            foreach (int i in nums)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static IEnumerable<int> GenerateNumbers(int maxNum)
+        {
+            for (int i = 0; i < maxNum; i++)
+            {
+                yield return i; // everytime when i in nums of foreach is accessed, yield return is called/reached out to get the value of i
+            }
+        }
+    }
+}
