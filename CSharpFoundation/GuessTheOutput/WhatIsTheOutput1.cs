3679943957785428f5bc70f2c799b898c8d3c95d
King_Jireh <samuelsherpa46@gmail.com>
2024-12-03 10:42:41 +0545

    watch the flow of for int

---
+++ CSharpFoundation/GuessTheOutput/WhatIsTheOutput1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFoundation.GuessTheOutput
+{
+    public class WhatIsTheOutput1
+    {
+        public static void Main(string[] args)
+        {
+            int J = 0;
+            for (int I = 2; I <= 10; I +=2)
+            {
+                J += I;
+            }
+            Console.WriteLine("The total sum is: {0}",J);
+            Console.ReadLine();
+        }
+    }
+}
